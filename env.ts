88bcdbbab49a0d541fbaf399a11ce02216cdcbf0
nehemie-n <nehemie054@gmail.com>
2023-10-16 12:10:26 +0200

    feat: configured ThrottlerModule with redis

---
+++ env.ts
@@ -0,0 +1,4 @@
+// JWT_SECRET = string;
+
+// RATE_LIMIT_TTL = 10;
+// RATE_LIMIT_MAX = 1000;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule, seconds } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ClientsModule } from './clients/clients.module';
 import { DbModule } from './db/db.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,6 +16,29 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     ClientsModule,
     DbModule,
+    /**
+     *
+     * The default configuration uses an in Memory cache
+     * For distributed servers we Redis to have a single source of truth since we have distributed computing nodes.
+     */
+    ThrottlerModule.forRoot({
+      /**
+       * Default config
+       * (host = localhost, port = 6379)
+       */
+      storage: new ThrottlerStorageRedisService(),
+      /**
+       * When using redis clusters
+       */ // storage: new ThrottlerStorageRedisService(
+      //   new Redis.Cluster(nodes, options),
+      // ),
+      throttlers: [
+        {
+          ttl: seconds(parseInt(process.env.RATE_LIMIT_TTL)),
+          limit: parseInt(process.env.RATE_LIMIT_MAX!),
+        },
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
